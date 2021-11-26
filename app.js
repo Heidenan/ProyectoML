b45dc8ef025c6224cb0c046536c2469875afae75
Heidenan <jbradach26@gmail.com>
2021-11-25 22:11:51 -0300

    Agregue formularios y paginas

--- app.js
+++ app.js
@@ -10,3 +10,5 @@ app.listen(app.get("port"), () =>
 
 app.get('/', (req,res) =>   res.sendFile(path.resolve(__dirname,'./views/home.html')))
 app.use(express.static(path.resolve(__dirname,'./public')))
+app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname,'./views', 'register.html')))
+app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname,'./views', 'login.html')))
